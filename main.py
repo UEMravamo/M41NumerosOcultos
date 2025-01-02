40844fa6c167136114c0a7ece4ab03dda5598471
Geronimo <gerobasso01@gmail.com>
2025-01-02 17:48:36 +0100

    Function to convert any base into decimal base

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+def n_base_decimal_base(numbers, base):
+    value_converted = 0
+    for i, number in enumerate(numbers):
+        value_converted += (numbers[i] * (base ** (len(numbers) - i - 1)))
+    return value_converted
+
+
 if __name__ == '__main__':
     print('Hello World')
